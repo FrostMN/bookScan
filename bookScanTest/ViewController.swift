7dc53d52807557f07cd215e008ee202af4fcf000
aaron <asouer@gmail.com>
2017-11-14 17:23:02 -0600

    Initial Commit

---
+++ bookScanTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bookScanTest
+//
+//  Created by Aaron Souer on 11/14/17.
+//  Copyright © 2017 Aaron Souer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
